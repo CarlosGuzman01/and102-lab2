79a0eb81f775b3abe2e2565a54da683252daac17
CarlosGuzman01 <carlosguzmantejeda13@gmail.com>
2024-03-08 14:29:19 -0500

    done

--- app/src/main/java/com/example/and102_lab2/MainActivity.kt
+++ app/src/main/java/com/example/and102_lab2/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.and102_lab2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var emails: List<Email>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val recyclerView = findViewById<RecyclerView>(R.id.emailRv)
+
+        val list = EmailFetcher.getEmails()
+
+        val emailAdapter = EmailAdapter(list)
+
+        recyclerView.adapter = emailAdapter
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
